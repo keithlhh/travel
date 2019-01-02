ba33f4122434c9c0022a5bf23d32fbf3b4fd061b
lihonghui <lihonghui_lhh@163.com>
2019-01-02 23:47:47 +0800

    travel commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+   <home-header></home-header>
+   <home-swiper :list="swiperList"></home-swiper>
+   <home-icons :list="iconList"></home-icons>
+   <home-recommend :list="recommendList"></home-recommend>
+   <home-weekend :weekendList="weekendList"></home-weekend>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+import { mapState } from 'vuex'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  computed: {
+    ...mapState(['city'])
+  },
+  data () {
+    return {
+      lastCity: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json?city=' + this.city)
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      const resp = res.data
+      if (resp.ret && resp.data) {
+        this.swiperList = resp.data.swiperList
+        this.iconList = resp.data.iconList
+        this.recommendList = resp.data.recommendList
+        this.weekendList = resp.data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="wrapper">
+<swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of list" :key="item.id" v-if="show">
+      <img class="swiper-img" :src="item.imgUrl"/>
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 1000,
+        effect: 'fade'
+      }
+    }
+  },
+  computed: {
+    show () {
+      return this.list.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+      background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    background: #eee
+    padding-bottom: 26.6%
+    .swiper-img
+      width: 100%
+</style>
